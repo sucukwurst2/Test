b273ff74eee16f1c846fd534a075dc6da5c34bf6
Niki <Niki@DESKTOP-GT37UIV>
2018-03-05 19:32:34 +0100

    asasasas

--- src/at/spengergasse/connect/Runner.java
+++ src/at/spengergasse/connect/Runner.java
@@ -9,12 +9,16 @@ import javafx.scene.*;
 import javafx.scene.control.Button;
 import javafx.scene.image.*;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.BackgroundImage;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.shape.Shape;
 import javafx.stage.Stage;
-
+import java.io.DataOutputStream;
+import java.io.ObjectInputStream;
+import java.net.Socket;
 
 /**
  * https://carlfx.wordpress.com/tag/collision-detection/   
@@ -22,50 +26,62 @@ import javafx.stage.Stage;
 public class Runner extends Application {
 	
 	
-    private double W = 600, H = 400;
-    private ArrayList<Wall> walls = new ArrayList();
+    private double W = 1000, H = 666;
+
 
     boolean running, goNorth, goSouth, goEast, goWest;
     private static final String IMG_1 =
-            "file:///C:/Users/Niki/git/Test/img/player1.png", IMG_2 = "file:///C:/Users/Niki/git/Test/img/player2.png";
+            "file:///C:/Users/Niki/git/Test/img/player1.png", IMG_2 = "file:///C:/Users/Niki/git/Test/img/player2.png",BACKGROUND = "file:///C:/Users/Niki/git/Test/img/boden.png";
 
     private Image playerImage1,playerImage2,image;
     private Node  player1;
     private Node  player2;
     private static Group root;
+    private ArrayList<Wall> walls = new ArrayList();
     
-    
-    
-	private Node rect;
-	private static final String imgpath = "file:///C:/Users/Niki/git/Test/img/wall.png";
 
     @Override
     public void start(Stage stage) throws Exception {
-    	//player1 = new Circle(50.0f, Color.RED);
-		//movePlayer1To(100, 100);
+
         playerImage1 = new Image(IMG_1);
         player1 = new ImageView(playerImage1);
         
         playerImage2 = new Image(IMG_2);
         player2 = new ImageView(playerImage2);
-        
-        image = new Image(imgpath);
-		rect = new ImageView(image);
-        
-        root = new Group(player1, player2,rect);
-        
-        //Wall wall1 = new Wall(350,350,this);
-        //root = wall1.returnGroup();
 
+        
+        image = new Image(BACKGROUND);
+        ImageView imgView = new ImageView(image);
+		stage.setResizable(false);
+        
+        root = new Group();
+        root.getChildren().add(imgView);
+        root.getChildren().add(player1);
+        root.getChildren().add(player2);
+        
+        walls.add(new Wall(W/8,H/8));
+        walls.add(new Wall(500,500));
+       
+
+		
+		
+		
+
+		Scene scene1 = new Scene(root, W, H);
+		
 
 
 		
-		stage.setResizable(false);
-		Scene scene1 = new Scene(root, W, H, Color.FORESTGREEN);
+		
+		
+		for(int x = 0;x<walls.size();x++) {
+			Wall w = walls.get(x);
+			StackPane y = walls.get(x).getWall();
+			root.getChildren().add(y);
+			y.relocate(w.getX(),w.getY());
 
+		}
 
-       
-		rect.relocate(W/8, H/8);
         player1.relocate(W/2,H/2 );
         player2.relocate(W/4, H/4);
 
@@ -106,17 +122,17 @@ public class Runner extends Application {
             public void handle(long now) {
                 int dx = 0, dy = 0;
 
-                if (goNorth && checkPlayerUp(player2)!=true && checkPlayerUp(rect)!=true) dy -= 1;
-                if (goSouth && checkPlayerDown(player2)!=true && checkPlayerDown(rect)!=true) dy += 1;
-                if (goEast && checkPlayerRight(player2)!=true && checkPlayerRight(rect)!=true)  dx += 1;
-                if (goWest && checkPlayerLeft(player2)!=true && checkPlayerLeft(rect)!=true)  dx -= 1;
+                if (goNorth && checkUp()!=true) dy -= 1;
+                if (goSouth && checkDown()!=true) dy += 1;
+                if (goEast && checkRight()!=true)  dx += 1;
+                if (goWest && checkLeft()!=true)  dx -= 1;
                 if (running) { dx *= 3; dy *= 3; }
                 
 
                 movePlayerBy(dx, dy,player1);
-               // System.out.println(checkPlayerCollision(player1, player2));
-                //System.out.println(player1.getBoundsInParent());
-                System.out.println("Left:" + checkPlayerLeft(player2) + "    Right:" + checkPlayerRight(player2) + "    Up:" + checkPlayerUp(player2) + "    Down:" + checkPlayerDown(player2));
+                //serverStuff(1, 1);
+              
+                System.out.println("Left:" + checkLeft() + "    Right:" + checkRight() + "    Up:" + checkUp() + "    Down:" + checkDown());
                 
             }
         };
@@ -185,13 +201,23 @@ public class Runner extends Application {
         }
     }
     
-    public boolean checkPlayerCollision(Node player1, Node player2) {
+    /*public boolean checkPlayerCollision(Node player1, Node player2) {
     	if(player1.getBoundsInParent().intersects(player2.getBoundsInParent()))return true;
     	else return false;
-
     	
     }
     
+    public void checkSet(double x,double y,Node n) {
+    	double xx = player1.getBoundsInParent().getMinX()+25;
+    	double yy = player1.getBoundsInParent().getMinY()+25;
+    	player1.relocate(x, y);
+    	if(player1.getBoundsInParent().intersects(player2.getBoundsInParent())){
+    		player1.relocate(xx, yy);
+    	}
+    }*/
+    
+    
+ 
     public boolean checkPlayerUp(Node n) {
     	if((player1.getBoundsInParent().getMinY() == n.getBoundsInParent().getMaxY()+1) &&
     	    player1.getBoundsInParent().getMaxX() >= n.getBoundsInParent().getMinX() &&
@@ -210,16 +236,110 @@ public class Runner extends Application {
     public boolean checkPlayerLeft(Node n) {
     	if(player1.getBoundsInParent().getMinX() == n.getBoundsInParent().getMaxX()+1 &&
     	   player1.getBoundsInParent().getMaxY() >= n.getBoundsInParent().getMinY() &&
-    	   player1.getBoundsInParent().getMaxY() <= n.getBoundsInParent().getMaxX())return true;
+    	   player1.getBoundsInParent().getMinY() <= n.getBoundsInParent().getMaxX())return true;
     	else return false;
     }
     public boolean checkPlayerRight(Node n) {
     	if(player1.getBoundsInParent().getMaxX() == n.getBoundsInParent().getMinX()-1 &&
     	   player1.getBoundsInParent().getMaxY() >= n.getBoundsInParent().getMinY() &&
-    	   player1.getBoundsInParent().getMaxY() <= n.getBoundsInParent().getMaxX())return true;
+    	   player1.getBoundsInParent().getMinY() <= n.getBoundsInParent().getMaxX())return true;
     	else return false;
     }
     
+    
+    
+    public boolean checkWallUp(Wall w) {
+    	if((player1.getBoundsInParent().getMinY() == w.getWall().getBoundsInParent().getMaxY()+1) &&
+    	    player1.getBoundsInParent().getMaxX() >= w.getWall().getBoundsInParent().getMinX() &&
+    	    player1.getBoundsInParent().getMinX() <= w.getWall().getBoundsInParent().getMaxX())return true;
+    	else return false;
+    	
+    			
+    }
+    public boolean checkWallDown(Wall w) {
+    	if((player1.getBoundsInParent().getMaxY() == w.getWall().getBoundsInParent().getMinY()-1 &&
+    	    player1.getBoundsInParent().getMaxX() >= w.getWall().getBoundsInParent().getMinX() &&
+    	    player1.getBoundsInParent().getMinX() <= w.getWall().getBoundsInParent().getMaxX()))return true;
+    	else return false;
+    	
+    }
+    public boolean checkWallLeft(Wall w) {
+    	if(player1.getBoundsInParent().getMinX() == w.getWall().getBoundsInParent().getMaxX()+1 &&
+    	   player1.getBoundsInParent().getMaxY() >= w.getWall().getBoundsInParent().getMinY() &&
+    	   player1.getBoundsInParent().getMinY() <= w.getWall().getBoundsInParent().getMaxX())return true;
+    	else return false;
+    }
+    public boolean checkWallRight(Wall w) {
+    	if(player1.getBoundsInParent().getMaxX() == w.getWall().getBoundsInParent().getMinX()-1 &&
+    	   player1.getBoundsInParent().getMaxY() >= w.getWall().getBoundsInParent().getMinY() &&
+    	   player1.getBoundsInParent().getMinY() <= w.getWall().getBoundsInParent().getMaxX())return true;
+    	else return false;
+    }
+    
+    
+    
+    public boolean checkRight() {
+    	boolean b = false;
+    	if(checkPlayerRight(player2))b = true;
+    	for(int x = 0;x<walls.size();x++) {
+			Wall w = walls.get(x);
+			
+			if(checkWallRight(w))b = true;
+		}
+    	return b;
+    }
+    
+    public boolean checkLeft() {
+    	boolean b = false;
+    	if(checkPlayerLeft(player2))b = true;
+    	for(int x = 0;x<walls.size();x++) {
+			Wall w = walls.get(x);
+			
+			if(checkWallLeft(w))b = true;
+		}
+    	return b;
+    }
+    public boolean checkUp() {
+    	boolean b = false;
+    	if(checkPlayerUp(player2))b = true;
+    	for(int x = 0;x<walls.size();x++) {
+			Wall w = walls.get(x);
+			
+			if(checkWallUp(w))b = true;
+		}
+    	return b;
+    }
+    public boolean checkDown() {
+    	boolean b = false;
+    	if(checkPlayerDown(player2))b = true;
+    	for(int x = 0;x<walls.size();x++) {
+			Wall w = walls.get(x);
+			
+			if(checkWallDown(w))b = true;
+		}
+    	return b;
+    }
+    
+    
+    
+    
+    
+    /*
+    public void serverStuff(int x, int y) {
+    	try {
+
+    	     Socket socketConnection = new Socket("127.0.0.1", 11111);
+
+
+    	     //QUERY PASSING
+    	     DataOutputStream outToServer = new DataOutputStream(socketConnection.getOutputStream());
+    	     
+    	     int SQL= 2 + 3;
+    	     outToServer.write(SQL);
+
+
+    	  } catch (Exception e) {System.out.println(e); }
+     }*/
     
     
     
@@ -228,17 +348,6 @@ public class Runner extends Application {
 
     
     
-    
-    
-    
-    
-	public ArrayList<Wall> getWalls() {
-		return walls;
-	}
-
-	public void addWalls(Wall wallss) {
-		walls.add(wallss);
-	}
 	
 
 	public  double getW() {
--- src/at/spengergasse/connect/Wall.java
+++ src/at/spengergasse/connect/Wall.java
@@ -3,29 +3,33 @@ package at.spengergasse.connect;
 
 
 import javafx.scene.image.*;
-import javafx.scene.*;
-
-public class Wall{
-	private double x,y;
-	private Node rect;
+import javafx.scene.layout.StackPane;
+import javafx.scene.shape.Rectangle;
+/**
+ * 
+ * 
+ * @author Niki
+ * erstellt ein Rectangle auf das ein bild gelegt wird, 2 variablen um die x und y koordinaten zu speichern
+ */
+public class Wall {
+	private Rectangle rect;
 	private static final String imgpath = "file:///C:/Users/Niki/git/Test/img/wall.png";
-	private Image img;
-	private Runner r;
-	Group g;
+	private StackPane wall;
+	private ImageView img;
+	private double x,y;
 	
-	public Wall(double x,double y,Runner r) {
-		this.r = r;
-		img = new Image(imgpath);
-		rect = new ImageView(img);
-		
-		
-		
+	public Wall(double x, double y){
+	    rect = new Rectangle(30,30);
+        wall = new StackPane();
+        img = new ImageView(imgpath);
+        wall.getChildren().addAll(rect, img);
+        setX(x);
+        setY(y);
+        
 	}
-	
-	public Group returnGroup() {
-		return g;
+	public StackPane getWall() {
+		return wall;
 	}
-	
 	public double getX() {
 		return x;
 	}
@@ -38,14 +42,8 @@ public class Wall{
 	public void setY(double y) {
 		this.y = y;
 	}
-
-	public Node getRect() {
-		return rect;
-	}
-
-	public void setRect(Node rect) {
-		this.rect = rect;
-	}
 	
 	
 }
+	
+
